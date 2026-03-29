7761af8982c3f3f073f5572f4529d96324219a7f
Daniel Chen <chendaniely@gmail.com>
2026-03-29 13:30:30 -0700

    create cherry blossoms geojson

---
+++ code/01-clean/cherry_blossoms.R
@@ -0,0 +1,24 @@
+library(sf)
+library(dplyr)
+library(stringr)
+
+# i have no idea what is an actual cherry blossom tree
+# there are a few tree common names that look like it will fit this description
+# this filtering affects all the trees that are plotted
+
+# --- Load raw trees ---
+trees <- st_read("data/original/public-trees.geojson")
+
+# --- Filter to Japanese flowering cherry varieties ---
+cherry_names <- unique(trees$common_name)[str_detect(
+  unique(trees$common_name),
+  "cherry|CHERRY"
+)]
+
+jap_flower_cherry <- cherry_names[str_detect(cherry_names, "JAP|jap|FLOWER|flower")]
+
+cherry_blossoms <- trees %>%
+  filter(common_name %in% jap_flower_cherry)
+
+# --- Save ---
+st_write(cherry_blossoms, "data/final/cherry_blossoms.geojson", delete_dsn = TRUE)
